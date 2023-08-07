d331df4f8e0b1ce0dbbf415759a1b40a61129f07
Pablo Garrido <pablogarrido219@gmail.com>
2023-08-07 15:40:56 -0500

    fix(responsive): Responsive for CSS and JS

--- set.js
+++ set.js
@@ -5,25 +5,22 @@ const stack = document.getElementById('stack2');
 const education = document.getElementById('education2');
 const projects = document.getElementById('projects2');
 
-//Botones de la pagina
-const mystack = document.getElementById("Pablostack")
-const aboutit = document.getElementById("PabloAbout")
-const profeedu = document.getElementById("PabloEdu")
-const proyects = document.getElementById("PabloPro")
-
-window.addEventListener('load', iniciarPortafolio);
-mystack.addEventListener("click",techstack);
-aboutit.addEventListener("click",iniciarPortafolio);
-profeedu.addEventListener("click",edu);
-proyects.addEventListener("click",proj);
+// Agrega una verificación para el ancho de la pantalla antes de asignar los eventos
+if (window.innerWidth < 767) {
+    mystack.addEventListener("click", techstack);
+    aboutit.addEventListener("click", iniciarPortafolio);
+    profeedu.addEventListener("click", edu);
+    proyects.addEventListener("click", proj);
+}
 
 function iniciarPortafolio() {
     stack.style.display = "none";
     education.style.display = "none";
     projects.style.display = "none";
-    title.style.display="block"
-    about.style.display="block"
+    title.style.display = "block";
+    about.style.display = "block";
 }
+
 function techstack(){
     about.style.display = "none";
     education.style.display = "none";
@@ -31,6 +28,7 @@ function techstack(){
     title.style.display = "none";
     stack.style.display = "block"; // Mostrar la sección "Tech Stack"
 }
+
 function edu(){
     about.style.display = "none";
     stack.style.display = "none";
@@ -38,6 +36,7 @@ function edu(){
     title.style.display = "none";
     education.style.display = "block";
 }
+
 function proj(){
     about.style.display = "none";
     education.style.display = "none";
